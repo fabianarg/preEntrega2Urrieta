4e250ac0de493bc5dbcc908844f2933372b39c50
Fabiana Urrieta <judibanaa@gmail.com>
2023-07-10 23:51:56 -0300

    Misma logica, nuevo codigo js

--- main.js
+++ main.js
@@ -1,37 +1,57 @@
 //Array de objetos inicial
 const personas = [
-  {nombre: "marcos", edad: 32, peso: 85, estatura: 178},
-  {nombre: "maria", edad: 26, peso: 65, estatura: 159},
-  {nombre: "julieta", edad: 24, peso: 70, estatura: 160},
-  {nombre: "vanessa", edad: 61, peso: 70, estatura: 158},
-  {nombre: "diego", edad: 63, peso: 90, estatura: 180},
+  { nombre: "marcos", edad: 32, peso: 85, estatura: 178 },
+  { nombre: "maria", edad: 26, peso: 65, estatura: 159 },
+  { nombre: "julieta", edad: 24, peso: 70, estatura: 160 },
+  { nombre: "vanessa", edad: 61, peso: 70, estatura: 158 },
+  { nombre: "diego", edad: 63, peso: 90, estatura: 180 },
 ];
 
-//funcion para agregar a una nueva persona en el array inicial
-function agregarPersona() {
-  class Persona {
-    constructor(nombre, edad, peso, estatura) {
-      this.nombre = nombre;
-      this.edad = edad;
-      this.peso = peso;
-      this.estatura = estatura;
+function consultarNombreCensado(array, propiedad, valor) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i][propiedad] === valor) {
+      return array[i];
     }
-  
-    agregarAlArray() {
-      personas.push(this);
-    }
+  }
+  return null;
+}
+
+let nombreBuscado = prompt(
+  "Ingrese el nombre de la persona que busca en el censo:"
+);
+let personaEncontrada = consultarNombreCensado(
+  personas,
+  "nombre",
+  nombreBuscado
+);
+
+if (personaEncontrada !== null) {
+  let mensaje = `
+    Se encontró la siguiente coincidencia:
+    Nombre: ${personaEncontrada.nombre}
+    Edad: ${personaEncontrada.edad}
+    Peso: ${personaEncontrada.peso}
+    Estatura: ${personaEncontrada.estatura}
+  `;
+  alert(mensaje);
+} else {
+  alert(
+    "El nombre ingresado no corresponde con una persona que haya sido censada. Ingrese los datos que se solicitan a continuación para agregar nueva persona al censo"
+  );
+
+  // Pedir al usuario los valores para el nuevo objeto
+  let nuevoNombre = prompt("Ingrese el nombre de la persona a censar:");
+  let nuevaEdad = prompt("Ingrese la edad de la persona a censar:");
+  let nuevoPeso = prompt("Ingrese el peso de la persona a censar");
+  let nuevaEstatura = prompt("Ingrese la estatura de la persona a censar:");
+
+  // Crear el nuevo objeto
+  let nuevaPersona = {
+    nombre: nuevoNombre,
+    edad: parseInt(nuevaEdad),
+    peso: parseInt(nuevoPeso),
+    estatura: parseInt(nuevaEstatura),
   };
-
-  //Solicitar los datos al usuario para poder agregarlos
-  let nombre = prompt("Ingrese el nombre de la persona:");
-  let edad = parseInt(prompt("Ingrese la edad de la persona:"));
-  let peso = parseFloat(prompt("Ingrese el peso de la persona (en kg):"));
-  let estatura = parseFloat(prompt("Ingrese la estatura de la persona (en metros):"));
-
-  //Instanciar un objeto 
-  let nuevaPersona = new Persona(nombre, edad, peso, estatura);
-  nuevaPersona.agregarAlArray();
-  
   let mensajeNuevo = `
       ¡Los siguientes datos fueron agregados con éxito!
       Nombre: ${nuevaPersona.nombre}
@@ -40,20 +60,10 @@ function agregarPersona() {
       Estatura: ${nuevaPersona.estatura}
   `;
   alert(mensajeNuevo);
-};
 
-let dato = prompt("Consulte por nombre");
-const persona = personas.find((p) => p.nombre.toLowerCase() === dato.toLowerCase());
-  if (persona) {
-    let mensaje = `
-      Se encontró la siguiente coincidencia:
-      Nombre: ${persona.nombre}
-      Edad: ${persona.edad}
-      Peso: ${persona.peso}
-      Estatura: ${persona.estatura}
-    `;
-    alert(mensaje);
-  } else {
-    alert("El nombre ingresado no corresponde con una persona censada");
-    agregarPersona();
-  };
+  // Agregar el nuevo objeto al array
+  personas.push(nuevaPersona);
+  console.log("Nuevo objeto agregado:", nuevaPersona);
+}
+
+console.log("Array actualizado:", personas);
